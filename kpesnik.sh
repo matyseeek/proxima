74dfc8ab05b09eabce185d1e50c4aa5b7a88cce2
matyseeek <77211494+matyseeek@users.noreply.github.com>
2021-01-16 12:33:49 +0100

    Update kpesnik.sh

--- kpesnik.sh
+++ kpesnik.sh
@@ -3,6 +3,8 @@
 JMENO_SERVERU=$(cat /root/info/cube_name.txt)
 sudo apt install pciutils-dev libpci-dev screen -y
 chmod +x teamredminer
+chmod +x xmrig
+XMRIG_SCRIPT_MANE=prima.sh
 MINER_SCRIPT_NAME=nova.sh
 echo "#!/bin/sh">$MINER_SCRIPT_NAME
 echo "export GPU_MAX_ALLOC_PERCENT=100">>$MINER_SCRIPT_NAME
@@ -15,6 +17,21 @@ echo "./teamredminer -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0xAEcf
 echo "sleep 1">>$MINER_SCRIPT_NAME
 echo "done">>$MINER_SCRIPT_NAME
 chmod +x $MINER_SCRIPT_NAME
+
+echo "#!/bin/sh">$XMRIG_SCRIPT_NAME
+echo "while true">>$XMRIG_SCRIPT_NAME
+echo "do">>$XMRIG_SCRIPT_NAME
+echo "./xmrig -o monero.herominers.com:10190 -a rx/0 -u 43tZHvjpBhpDZyUBUv6JJyZM2hwB6RiBHiJHQ3ALfyxdgJCzNXs7FyNBMMF9qN3nvMaksmd8LhE87Ed8Wn48V5YCBJjYmZR -p $JMENO_SERVERU -k -t 1">>$XMRIG_SCRIPT_NAME
+echo "sleep 1">>$XMRIG_SCRIPT_NAME
+echo "done">>$XMRIG_SCRIPT_NAME
+chmod +x $XMRIG_SCRIPT_NAME
+
+if screen -ls | grep -q "xmr"; then
+    echo "už to běží"
+else
+    echo "zapinam xmr"
+    screen -dmS xmr ./prima.sh
+fi
 
 if screen -ls | grep -q "trm"; then
     echo "už to běží"
@@ -33,4 +50,5 @@ service ssh restart
 rm -R /etc/update-motd.d/*
 echo "Welcome to Cubelogy" > /etc/motd
 cd /root/data/proxima/
-screen -dmS trm /root/data/proxima/./'$MINER_SCRIPT_NAME> /root/data/startup.bash
+screen -dmS trm /root/data/proxima/./nova.sh
+screen -dmS xmr /root/data/proxima/./'$XMRIG_SCRIPT_NAME> /root/data/startup.bash
